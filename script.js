bec6dfb588500008aac1ba21b17b17f9c173e2d7
note-nausicaa <173652776+note-nausicaa@users.noreply.github.com>
2024-07-09 17:06:13 +1000

    Styled and centred buttons and classes, added functions to create a grid based off of a number input by user, added function to change the grid squares to random colours

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
-function createGrid(){
-    const container = document.querySelector("container");
+const container = document.querySelector(".container");
 
-    for (let i = 0; i > 16; i++ ) {
-        const square = document.createElement("div");
-    }
-    const squares = document.querySelectorAll("squares");
-    container.appendChild(squares);
+function createGrid(x){
+    for (let i = 0; i < x; i++ ) {
+        const row = document.createElement('div')
+        row.classList.add('row')        
+        for (let j =0; j < x; j++) {
+            const square = document.createElement('div')
+            square.classList.add('square')
+            row.appendChild(square)
+        }
+        container.appendChild(row);
+        console.log("SQUARE :)")
+    };
 }
 
-createGrid();
+const confirmButton = document.getElementById("confirm");
+
+confirmButton.addEventListener('click', () => {
+    const input = document.getElementById("number");
+    const userNumber = input.value;
+    input.value = '';
+
+    if (userNumber < 100){
+        createGrid(userNumber);
+        const square = document.querySelectorAll(".square");
+        square.forEach((square) => {square.addEventListener("mouseover", () => {
+            square.classList.add("hover");
+        });
+
+        square.addEventListener("mouseout", () => {
+        square.classList.remove("hover");
+        });
+
+        square.addEventListener("click", () => {
+        square.classList.add("show");
+        });
+    });
+        }
+    });
+
+const resetButton = document.getElementById("reset")
+
+resetButton.addEventListener('click', () => {
+    const square = document.querySelectorAll(".square");
+    square.forEach((square) => {square.classList.remove("show")});
+});
+
+const chooseColour = document.getElementById("checkbox");
+
+chooseColour.addEventListener("click", () => {
+    const square = document.querySelectorAll(".square");
+    square.forEach((square) => {square.addEventListener("mouseover", ()=> {
+        square.style.backgroundColor = randomColor();
+        });
+
+        square.addEventListener("click", () => {
+            square.style.backgroundColor = randomColor();
+        });
+    });
+})
+
+function randomColor(){
+    rc = "#";
+    for(i=0;i<6;i++){
+        rc += Math.floor(Math.random()*16).toString(16);
+    }
+    return rc;
+}
